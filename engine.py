dc431da6f5ae746eb3fe74caee0ec204fc60f9b0
Vinicius_VM <96557487+Vinicius-VM@users.noreply.github.com>
2024-10-09 08:53:45 -0300

    Add files via upload

--- engine.py
+++ engine.py
@@ -2,12 +2,19 @@ import tcod as libtcod
 from input_handlers import handle_keys
 from entity import Entity
 from render_functions import clear_all, render_all
+from map_objects.game_map import GameMap
 
 def main():
     screen_width = 80
     screen_height = 50
     map_width = 80
     map_height = 45
+    colors = {
+        'dark_wall': libtcod.Color(0, 0, 100),
+        'dark_ground': libtcod.Color(50, 50, 150)
+    }
+
+    #Aqui é para adicionar novos personagens, posteriormente vemos como editar
     player = Entity(int(screen_width / 2), int(screen_height / 2), '@', libtcod.white)
     npc = Entity(int(screen_width / 2 - 5), int(screen_height / 2), '@', libtcod.yellow)
     entities = [npc, player]
@@ -17,6 +24,7 @@ def main():
     libtcod.console_init_root(screen_width, screen_height, 'libtcod tutorial revised', False)
 
     con = libtcod.console_new(screen_width, screen_height)
+    game_map = GameMap(map_width, map_height)
 
     key = libtcod.Key()
     mouse = libtcod.Mouse()
@@ -24,7 +32,7 @@ def main():
     while not libtcod.console_is_window_closed():
         libtcod.sys_check_for_event(libtcod.EVENT_KEY_PRESS, key, mouse)
 
-        render_all(con, entities, screen_width, screen_height)
+        render_all(con, entities, game_map, screen_width, screen_height, colors)
 
         libtcod.console_flush()
 
@@ -38,7 +46,9 @@ def main():
 
         if move:
             dx, dy = move
-            player.move(dx, dy)
+            if not game_map.is_blocked(player.x + dx, player.y + dy):
+                player.move(dx, dy)
+
 
         if exit:
             return True
